ec7c37a2c3e63e6c15220cef252d71fb43f1ab44
Li Shihao <me@shihao.li>
2019-06-06 07:39:27 +0800

    used dein

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 # /bin/bash
-git submodule update --init --recursive
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+chmod +x installer.sh && sh ./installer.sh ~/.cache/dein
+
 source ~/.zshrc
-vim -c 'call dein#install()'
+vim -c call dein#install() -c UpdateRemotePugins -c quitall 
 tmux source ~/.tmux.conf
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
